d0cf55a5fa975b31183bf99b17df06ff5f79e7cf
Karl <karletsebeth@gmail.com>
2022-07-06 12:23:13 +0200

    Basic music control

--- FrontEnd/Scripts/GameLogic.js
+++ FrontEnd/Scripts/GameLogic.js
@@ -88,7 +88,115 @@ function requestPermissions()
     //Take away request permission button
     document.getElementById("reqPerm").style.display="none";
 
-    //Wait for flashlight to turn on
+    //Music for host
+    var isHost = localStorage.getItem("host");
+    if(isHost)
+    {
+        var countDownAudio, mainAudio, currentTime, startTime;
+        
+        countDownAudio = new Audio();
+        countDownAudio.src = 'https://res.cloudinary.com/dtd0lxvsg/video/upload/v1657094095/321Kratos_ncov62.mp3';
+        document.body.appendChild(countDownAudio);
+        
+        mainAudio = new Audio();
+        mainAudio.src = 'https://res.cloudinary.com/dtd0lxvsg/video/upload/v1657093868/DustCropped_lyk92b.mp3';
+        document.body.appendChild(mainAudio);
+        
+        var running = false;
+        var mainAudioPlaying = false;
+        var startTime;
+        var currentTime;
+        var playBackRate;
+        var timer = 0;
+        var today = new Date();
+        
+        function modifyTimer()
+        {
+            var today = new Date();
+            this.currentTime = today.getMinutes() * 60 + today.getSeconds();
+            if (this.currentTime - this.startTime > 0 & mainAudioPlaying) //we only want to change the time if it is at least 1 second
+                this.timer = this.timer + 1;
+            this.startTime = this.currentTime; //resets the time
+        }
+            
+        if (!running)
+        {
+            running = true;
+            startTime = today.getMinutes() * 60 + today.getSeconds();
+            running = true;
+            countDownAudio.play();
+    
+            function renderFrame()
+            {
+                if (running)
+                {
+                    requestAnimationFrame(renderFrame);
+                    modifyTimer();
+                    // elapsedTime = Math.trunc((currentTime - startTime) % mainAudio.duration);
+                    switch (timer)
+                    {
+                        case 13:
+                            playBackRate = 1.2;
+                            mainAudio.playbackRate = playBackRate;
+                            break;
+                        case 18:
+                            playBackRate = 1;
+                            mainAudio.playbackRate = playBackRate;
+                            break;
+                        case 28:
+                            playBackRate = 0.8;
+                            mainAudio.playbackRate = playBackRate;
+                            break;
+                        case 34:
+                            playBackRate = 1;
+                            mainAudio.playbackRate = playBackRate;
+                            break;
+                        case 40:
+                            playBackRate = 1.4;
+                            mainAudio.playbackRate = playBackRate;
+                            break;
+                        case 46:
+                            playBackRate = 1;
+                            mainAudio.playbackRate = playBackRate;
+                            break;
+                        case 49:
+                            playBackRate = 0.6;
+                            mainAudio.playbackRate = playBackRate;
+                            break;
+                        case 55:
+                            playBackRate = 1;
+                            mainAudio.playbackRate = playBackRate;
+                            break;
+                        case 67:
+                            playBackRate = 1.3;
+                            mainAudio.playbackRate = playBackRate;
+                            break;
+                        case 73:
+                            playBackRate = 1;
+                            mainAudio.playbackRate = playBackRate;
+                            break
+                    }
+                    console.log(timer);
+                }
+            }
+            renderFrame();
+        }
+        else
+        {
+            mainAudio.pause();
+            mainAudioPlaying = false;
+            running = false;
+        }
+        
+        mainAudio.onended = function() {
+            mainAudio.play();
+        }
+        
+        countDownAudio.onended = function() {
+            mainAudioPlaying = true;
+            mainAudio.play();
+        } 
+    }
 }
 
 function startSensors(){
