7f5d19bce72262a34613f6e430ed33fef88f9672
Gerni <gerni.visser@gmail.com>
2022-07-06 09:27:09 +0200

    updated backend

--- WsServer.js
+++ WsServer.js
@@ -1,133 +1,152 @@
-/*
-* User Class that stores the socket and the status of the user in the current game.
-*/
-class User {
-  constructor(socket){
-      this.socket = socket
-      this.alive = true
-  }
+const sessions = []
+// Gernerates new session, adds to sessions list and returns sessionId
+function generateSession(playerCount = 5){
+  let session = {
+    players: [],
+    maxPlayerCount: playerCount,
+    alivePlayerCount: function () {
+      var aliveCount = 0
+      for (let index = 0; index < this.players.length; index++) {
+        const element = this.players[index];
+        if(element.alive){
+          aliveCount ++
+        }
+      }
+      return aliveCount
+    }, 
+    sessionId: function () {
+      let sesId = 'AAA'
 
-  kill(){
-      this.alive = false
-  }
+      //Check SessionID is unique
+      while(sessions.find((session) => session.sessionId == sesId)){
+        sesId = (Math.random() + 1).toString(36).substring(9)
+      }
 
-  reset() {
-    this.alive = true;
+      return sesId
+    }()
+  }
+  sessions.push(session)
+  
+  return session.sessionId
+}
+
+function findSession(ws,sessionId){
+  const session = sessions.find((session) => session.sessionId == sessionId)
+  if(!session){
+    ws.send(JSON.stringify({
+      "type" : "join",
+      "status" : "Failed: Session Does Not Exist!"
+    }));
+
+    return
+  }
+  else{
+    return session
   }
 }
 
-/*
-* Session Class to encapsulate the Users playing and associate them with a session for the
-* current game they are playing.
-*/
-class Session {
-  constructor(sessionId, userCount){
-      this.sessionId = sessionId
-      this.userCount = userCount
-      this.users = []
-      this.activeUsers = 0;
+function findPlayer(ws,session){
+  const player = session.players.find((player) => player.ws == ws)
+  if(!player){
+    return
   }
+  else{
+    return player
+  }
+}
 
-  AddUser(socket){
-    if(this.userCount >= this.users.length){
-        let user = new User(socket);
-        this.users.push(user)
-        this.activeUsers++;
+function addPlayerToSession(ws, sessionId){
+  const session = findSession(ws, sessionId)
+  if(session){
+    console.log(session.players.length);
+    console.log(session.maxPlayerCount);
+    if(session.players.length < session.maxPlayerCount){
+      if(findPlayer(ws,session)){
+        ws.send(JSON.stringify({
+          "type" : "join",
+          "status" : "Failed: Player aready Exist!"
+        }));
+  
+        return
+      }
+      else{
+        // Check player already joined
+        const newPlayer = {
+          ws: ws,
+          alive: true
+        }
+
+        session.players.push(newPlayer)
+        console.log("Sending data");
+        ws.send(JSON.stringify({
+          "type" : "playerCount",
+          "status" : "5"
+        }));
+      }
     }
     else{
-      socket.send(JSON.stringify({
+      ws.send(JSON.stringify({
         "type" : "join",
         "status" : "Failed: Lobby is Full!"
       }));
     }
   }
+}
 
-  KillUser(socket){
-      var curUser = this.users.find((user) => user.socket == socket)
-      curUser.kill();
+function killPlayer(ws, sessionId){
+  const session = findSession(ws,sessionId)
+  if(session){
+    const player = findPlayer(ws,session)
+    if(player){
+      //Kills player
+      player.alive = false
+      // Send conformation to player 
+      ws.send(JSON.stringify({
+        "type" : "kill",
+        "status" : "You are killed."
+      }));
+      // Check if only one player is left thus the winner
+      checkGameOver(session)
 
-      if (--this.activeUsers == 1) {
-        var curUser = this.users.find((user) => user.alive == true)
-        curUser.socket.send(JSON.stringify({
-          "type" : "win"
-        }));
-      }
-  }
-
-  removeUser(socket) {
-    this.KillUser(socket);
-    let curUser = this.users.find((user) => user.socket == socket);
-    this.users.splice(0,1);
-    
-  }
-
-  reset() {
-    for (let i=0; i<this.users.length; i++) {
-      this.users[i].reset();
+      return
     }
   }
 }
 
-/*
-* In memory storage of all currently active Sessions. Also acts as a controller to 
-* the Sessions.
-*/
-class SessionList {
-  constructor(){
-      this.sessions = []
-  }
-
-  print(sessionID) {
-    var curSession = this.sessions.find((session) => session.sessionId == sessionID)
-    for (let i=0; i< curSession.users.length; i++) {
-      console.log(curSession.users[i].alive);
-    }
-  }
-
-  KillUser(ws, sessionID) {
-    var curSession = this.sessions.find((session) => session.sessionId == sessionID)
-      if(!curSession){
-        // Implement Error Response
-        console.log("Session does not exist");
-        return 
+function checkGameOver(session){
+  if(session.alivePlayerCount() <= 1){
+    const winner = session.players.find((player) => player.alive == true)
+      // Message sent to losers
+      for (let index = 0; index < session.players.length; index++) {
+        var element = session.players[index];
+        if (element.ws == winner.ws){
+          element.ws.send(JSON.stringify({
+            "type" : "win",
+            "status" : "Wel done you are slightly above averagre compared to the rest"
+          }));
+        }
+        else{
+          element.ws.send(JSON.stringify({
+            "type" : "lose",
+            "status" : "Mission failed.. We'll get them next time troops"
+          }));
+        }
       }
-      else{
-        curSession.KillUser(ws);
-      }
-  }
-
-  AddSocketToSession(ws, sessionId){
-      var curSession = this.sessions.find((session) => session.sessionId == sessionId)
-      if(!curSession){
-        // Implement Error Response
-        ws.send(JSON.stringify({
-          "type" : "join",
-          "status" : "Failed: Session Does Not Exist!"
-        }));
-
-        return
-      }
-      else{
-        curSession.AddUser(ws)
-      }
-  }
-
-  AddSession(playerCount = 5){
-    let sessionid = 'AAA'
-    //Check SessionID is unique
-    while(this.sessions.find((session) => session.sessionId == sessionid)){
-      sessionid = (Math.random() + 1).toString(36).substring(9)
-    }
-
-    let session = new Session(sessionid, playerCount)
-    this.sessions.push(session)
-    return sessionid
   }
 }
 
+function removePlayer(ws, sessionId){
+  const session = findSession(ws,sessionId)
+  if(session){
+    const player = findPlayer(ws,session)
+    if(player){
+      killPlayer(ws, sessionId)
+      session.players.splice(session.players.indexOf(player),1)
+      return
 
-
-
+    }
+  }
+}
 /* ==========================================================
 * Servers
 ============================================================= */
@@ -150,8 +169,6 @@ console.log("http server listening on %d", port)
 var wss = new WebSocketServer({server: server})
 console.log("websocket server created")
 
-var Sessions = new SessionList();
-
 wss.on("connection", function(ws) {
 
   console.log("someone connected");
@@ -163,11 +180,11 @@ wss.on("connection", function(ws) {
     const type = obj.type;
 
     if (type == "join") {
-      Sessions.AddSocketToSession(ws, obj.sessionID);
+      console.log("Attempt join");
+      addPlayerToSession(ws, obj.sessionID)
     }
     else if (type == "lose") {
-      Sessions.KillUser(ws, obj.sessionID);
-      Sessions.print(obj.sessionID);
+      killPlayer(ws, obj.sessionID);
     }
     
   });
@@ -187,7 +204,7 @@ app.use(express.static(__dirname));
 app.post("/game/create",  function(request, response) {
 
   const count = request.body.playerCount;
-  var sesID = Sessions.AddSession(count);
+  var sesID = generateSession(count);
 
   console.log(count);
   console.log(sesID);
@@ -197,158 +214,4 @@ app.post("/game/create",  function(request, response) {
   }));
   
 });
-
-/*
-const express = require('express');
-const WebSocket = require('ws');
-const http = require('http');
-
-const Port = process.env.PORT || 8080;
-
-var app = express();
-const server = http.createServer(app);
-var wss = new WebSocket.Server({ server });
-
-var Sessions = new SessionList();
-
-wss.on('connection', (ws) => {
-
-  console.log("someone connected");
-
-  ws.on('message', function incoming(message) {
-  console.log('received: %s', message);
-
-    const obj = JSON.parse(message);
-    const type = obj.type;
-
-    if (type == "join") {
-      Sessions.AddSocketToSession(ws, obj.sessionID);
-    }
-    else if (type == "lose") {
-      Sessions.KillUser(ws, obj.sessionID);
-      Sessions.print(obj.sessionID);
-    }
-    
-  });
-
-  ws.on("close", function close() {
-
-  })
-
-});
-
-server.listen(Port, function() {
-  console.log("Server Listening On: " + Port);
-});
-
-const bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({ extended: false}));
-app.use(bodyParser.json());
-app.use(express.static('FrontEnd/'));
-app.use(express.static(__dirname));
-
-
-app.post("/game/create",  function(request, response) {
-
-  const count = request.body.playerCount;
-  var sesID = Sessions.AddSession(count);
-
-  console.log(count);
-  console.log(sesID);
-
-  response.end(JSON.stringify({
-    "sessionID" : sesID
-  }));
-  
-});
-*/
-
-/*
-const express = require('express');
-const WebSocket = require('ws');
-
-var app = express();
-const wss = new WebSocket.Server({ noServer:true });
-const Port = process.env.PORT || 8080;
-
-var server = app.listen(Port, function() {
-  console.log("Express WS Server Listening on Port " + Port);
-
-  server.on('upgrade', (request, socket, head) => {
-    console.log("upgrade");
-    wss.handleUpgrade(request, socket, head, socket => {
-      wss.emit('connection', socket, request);
-    });
-  });
-});
-
-
-var Sessions = new SessionList();
-
-wss.on('connection', function connection(ws) {
-
-  console.log("someone connected");
-
-  ws.on('message', function incoming(message) {
-  console.log('received: %s', message);
-
-    const obj = JSON.parse(message);
-    const type = obj.type;
-
-    if (type == "join") {
-      Sessions.AddSocketToSession(ws, obj.sessionID);
-    }
-    else if (type == "lose") {
-      Sessions.KillUser(ws, obj.sessionID);
-      Sessions.print(obj.sessionID);
-    }
-    
-  });
-
-  ws.on("close", function close() {
-
-  })
-
-});
-
-const bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({ extended: false}));
-app.use(bodyParser.json());
-app.use(express.static('FrontEnd/'));
-app.use(express.static(__dirname));
-
-
-app.post("/game/create",  function(request, response) {
-
-  const count = request.body.playerCount;
-  var sesID = Sessions.AddSession(count);
-
-  console.log(count);
-  console.log(sesID);
-
-  response.end(JSON.stringify({
-    "sessionID" : sesID
-  }));
-  
-});*/
-
-/* WebSocket Server */
-
-/*const { create } = require('domain');
-const fs = require('fs');
-const http = require('http');
-const { join } = require('path');
-
-
-const server = app.createServer();
-
-
-var clients = new Map();
-
-
-
-
-//server.listen(process.env.PORT);
-console.log("Listening on 8080");*/
-
 
